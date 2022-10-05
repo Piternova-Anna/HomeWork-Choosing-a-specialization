b6bd0a7e50ab8f55c708f76cb34af9aa189e38d7
Anna Piternova <anna-maria999@mail.ru>
2022-10-05 12:49:02 +0300

    finish

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[]Massiv = new string[5] { "one", "two", "three", "four", "five"};
+Console.Write("Дан массив из 5 элеменов:");
+Console.WriteLine("["+Massiv[0]+", "+Massiv[1]+", "+ Massiv[2]+", "+ Massiv[3]+", "+ Massiv[4]+"]");
+int quantity = 0;
+for (int i = 0; i < Massiv.Length; i++)
+{
+    if (Massiv[i].Length <= 3)
+    {
+        quantity++;
+    }
+}
+Console.WriteLine("в данном массиве элементов с количеством символов меньше или равное 3: "+quantity);
+
+string[] ResultMassiv = new string[quantity];
+int j = 0;
+Console.Write("новый массив: [");
+for (int i = 0; i < Massiv.Length; i++)
+{
+    if (Massiv[i].Length <= 3)
+    {
+        ResultMassiv[j] = Massiv[i];
+        
+        Console.Write(ResultMassiv[j]+",");
+        j++;
+    }
+}
+Console.WriteLine("]");
